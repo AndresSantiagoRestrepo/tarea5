79ecc5a58b7969fe177fe1cb8358d3ce0972f78f
Stokker <stokkerma@gmail.com>
2023-12-03 17:03:06 -0500

    Comentada la clase canción

--- src/main/java/caldas/Cancion.java
+++ src/main/java/caldas/Cancion.java
@@ -4,6 +4,19 @@ public class Cancion {
     String nombre;
     double duracion;
 
+    /**
+     * Constructor para las canciones, estas NO DEBEN tener el mismo
+     * nombre si están en el mismo album, pero no hay ningun problema
+     * si hay varias canciones con el mismo nombre en diferentes
+     * album
+     * 
+     * @param nombre   nombre de la cancion a crear, su nombre debe ser unico en
+     *                 cada album
+     * @param duracion duracion de la cancion a crear, esta debe estar en segundos,
+     *                 de esta forma una duracion de 360 significa que dura 6
+     *                 minutos
+     */
+
     public Cancion(String nombre, double duracion) {
         this.nombre = nombre;
         this.duracion = duracion;
