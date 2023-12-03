1755003c3336e7022b90d0e1bda4aa0eed0dd285
Stokker <stokkerma@gmail.com>
2023-12-03 17:35:51 -0500

    Cometada la clase concierto

--- src/main/java/caldas/Concierto.java
+++ src/main/java/caldas/Concierto.java
@@ -7,6 +7,24 @@ public class Concierto {
     String hora;
     Double capacidad;
 
+    /**
+     * Constructor de concierto, este constructor asigna de forma normal todos los
+     * parametros que recibe
+     * 
+     * @param nombre    Nombre del concierto que se desea orgnaizar, este nombre no
+     *                  debe ser igual
+     *                  al de algun otro concierto que ya se haya registrado dentro
+     *                  de la lista de conciertos de la banda
+     * @param lugar     El lugar donde se desea organizar el concierto, puede ser
+     *                  cuelquier lugar del mundo,
+     *                  no hay restricciones para este parametro
+     * @param fecha     La fecha en que se realizará el concierto, de ser en formato
+     *                  DD/MM/AA
+     * @param hora      La hora de inicio del concierto, de ser en formato 24HH
+     * @param capacidad La capacidad total de personas que van a recibir en el
+     *                  concierto
+     */
+
     public Concierto(String nombre, String lugar, String fecha, String hora, double capacidad) {
         this.nombre = nombre;
         this.lugar = lugar;
