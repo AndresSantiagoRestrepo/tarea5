0d91969fb7e898a4f7f23b50666131d0f07fd28f
Stokker <stokkerma@gmail.com>
2023-12-03 16:31:08 -0500

    Comentada la clase AlbumException

--- src/main/java/caldas/AlbumException.java
+++ src/main/java/caldas/AlbumException.java
@@ -2,6 +2,11 @@ package caldas;
 
 public class AlbumException extends Exception{
     
+    /**
+     * Excepcion definida para manejar error al momento de querer realizar operaciones sobre elementos de album que no 
+     * existan o que sean null
+     * @param mensaje Mensaje de error deacuerdo a la operacion desde la que se active la excepcion
+     */
     public AlbumException (String mensaje){
         super(mensaje);
     }
